8dd23caa9c551c7af71e8627e6711d8a4fb09f14
Rafael Ferraro <ferraromv@gmail.com>
2021-07-06 20:32:09 -0300

    Adding missing filter

--- infra/repositories/orders_repository.rb
+++ infra/repositories/orders_repository.rb
@@ -1,11 +1,14 @@
 require 'json'
+require 'date'
 
 class OrdersRepository
   def initialize(source = nil)
     @source = source || JSON.parse(File.read('./dataset/orders.json'))
   end
 
-  def find_completed_orders
-    @source["RECORDS"].select { |order| !order["completed_at"].nil? }
+  def find_week_completed_orders
+    @source["RECORDS"].select do |order|
+      order["completed_at"] && DateTime.parse(order["completed_at"]) > (DateTime.now - 7)
+    end
   end
 end
--- spec/infra/repositories/orders_repository_spec.rb
+++ spec/infra/repositories/orders_repository_spec.rb
@@ -1,7 +1,7 @@
 require './infra/repositories/orders_repository.rb'
 
 RSpec.describe OrdersRepository do
-  context 'when asking for completed orders' do
+  context 'when asking for completed orders in the week' do
     it 'returns a list of them if exists' do
       json_list = {
         "RECORDS" => [
@@ -19,12 +19,20 @@ RSpec.describe OrdersRepository do
             "shopper_id" => "327",
             "amount" => "288.71",
             "created_at" => "01/01/2018 20:54:00",
-            "completed_at" => "03/01/2018 20:05:47"
+            "completed_at" => (DateTime.now - 2).strftime("%d/%m/%Y %H:%M:%S")
+          },
+          {
+            "id" => "2",
+            "merchant_id" => "8",
+            "shopper_id" => "327",
+            "amount" => "288.71",
+            "created_at" => "05/07/2021 20:54:00",
+            "completed_at" => (DateTime.now - 8).strftime("%d/%m/%Y %H:%M:%S")
           }
         ]
       }
 
-      result = OrdersRepository.new(json_list).find_completed_orders
+      result = OrdersRepository.new(json_list).find_week_completed_orders
 
       expect(result.length).to eq(1)
       expect(result.first["id"]).to eq("2")
@@ -52,7 +60,7 @@ RSpec.describe OrdersRepository do
         ]
       }
 
-      result = OrdersRepository.new(json_list).find_completed_orders
+      result = OrdersRepository.new(json_list).find_week_completed_orders
 
       expect(result.empty?).to eq(true)
     end
