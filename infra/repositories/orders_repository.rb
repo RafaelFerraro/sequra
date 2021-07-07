8140e352b516bd6390b0a0765406e507bbb85de9
Rafael Ferraro <ferraromv@gmail.com>
2021-07-07 12:45:34 -0300

    Removing unused comment

--- infra/repositories/orders_repository.rb
+++ infra/repositories/orders_repository.rb
@@ -4,7 +4,6 @@ require 'date'
 class OrdersRepository
   def self.find_week_completed_orders(source = JSON.parse(File.read('./dataset/orders.json')))
     source["RECORDS"].select do |order|
-      # TODO move to object
       !order["completed_at"].empty? && DateTime.parse(order["completed_at"]) > (DateTime.now - 7)
     end
   end
